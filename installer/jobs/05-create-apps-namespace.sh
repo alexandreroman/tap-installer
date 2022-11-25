5110604d68be5a05ad7e7f29719ffdc63165bbcc
Alexandre Roman <aroman@vmware.com>
2022-11-25 08:20:06 +0100

    Set up apps namespace

--- installer/jobs/05-create-apps-namespace.sh
+++ installer/jobs/05-create-apps-namespace.sh
@@ -10,16 +10,92 @@ set -e -o pipefail
 
 TAP_APPS_NS=${TAP_APPS_NS:-tap-apps}
 
-if [ ! -z "$(yq '.jobs.skip[] | select(. == "create-apps-namespace")' $TAP_INSTALLER_CONFIG)" ]; then
-  echo "Skip creating apps namespace $TAP_APPS_NS"
-else
-  echo "Creating apps namespace $TAP_APPS_NS"
-cat << EOF | kapp deploy -c -y -a tap-ns-apps -f-
+NS_CONFIG=$(mktemp --suffix=.yaml)
+cat << EOF > "${NS_CONFIG}"
+#@ load("@ytt:data", "data")
+#@ load("@ytt:json", "json")
+---
 apiVersion: v1
 kind: Namespace
 metadata:
   name: $TAP_APPS_NS
+---
+#@ def config():
+#@   return {
+#@     "auths": {
+#@       data.values.registry.hostname: {
+#@         "username": data.values.registry.username,
+#@         "password": data.values.registry.password
+#@       }
+#@     }
+#@   }
+#@ end
+---
+apiVersion: v1
+kind: Secret
+metadata:
+  name: registry-credentials
+  namespace: $TAP_APPS_NS
+type: kubernetes.io/dockerconfigjson
+stringData:
+  .dockerconfigjson: #@ json.encode(config())
+---
+apiVersion: v1
+kind: Secret
+metadata:
+  name: tap-registry
+  namespace: $TAP_APPS_NS
+  annotations:
+    secretgen.carvel.dev/image-pull-secret: ""
+type: kubernetes.io/dockerconfigjson
+data:
+  .dockerconfigjson: e30K
+---
+apiVersion: v1
+kind: ServiceAccount
+metadata:
+  name: default
+  namespace: $TAP_APPS_NS
+secrets:
+- name: registry-credentials
+imagePullSecrets:
+- name: registry-credentials
+- name: tap-registry
+---
+apiVersion: rbac.authorization.k8s.io/v1
+kind: RoleBinding
+metadata:
+  name: default-permit-deliverable
+  namespace: $TAP_APPS_NS
+roleRef:
+  apiGroup: rbac.authorization.k8s.io
+  kind: ClusterRole
+  name: deliverable
+subjects:
+- kind: ServiceAccount
+  name: default
+---
+apiVersion: rbac.authorization.k8s.io/v1
+kind: RoleBinding
+metadata:
+  name: default-permit-workload
+  namespace: $TAP_APPS_NS
+roleRef:
+  apiGroup: rbac.authorization.k8s.io
+  kind: ClusterRole
+  name: workload
+subjects:
+- kind: ServiceAccount
+  name: default
 EOF
+
+if [ ! -z "$(yq '.jobs.skip[] | select(. == "create-apps-namespace")' $TAP_INSTALLER_CONFIG)" ]; then
+  echo "Skip creating apps namespace $TAP_APPS_NS"
+else
+  echo "Creating apps namespace $TAP_APPS_NS"
+  ytt -f "${TAP_INSTALLER_CONFIG}" \
+      -f "${NS_CONFIG}" | \
+    kapp deploy -c -y -a tap-ns-apps -f-
 fi
 
 # Kickstart next job.
